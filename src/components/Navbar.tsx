7c771d6fdc7a5cf72768edbe7cee3f61023f2aca
BinaryGuard26 <admin@binaryguard.ca>
2026-08-25 13:38:25 +0530

    Point website login icon to login.binaryguard.ca

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar({ currentPage, onNavigate }: NavbarProps) {
   };
 
   const openSecureAccess = () => {
-    window.location.assign('https://portal.binaryguard.ca');
+    window.location.assign('https://login.binaryguard.ca');
   };
 
   return (
